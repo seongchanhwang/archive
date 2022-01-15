92b2f407b2d971f7940e770109a3a169412a3e45
chan <tjdcksghkd12@gmail.com>
2022-01-16 00:52:54 +0900

    Initial commit

---
+++ src/test/java/com/portfolio/archive/ArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
